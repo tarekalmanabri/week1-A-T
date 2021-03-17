88dcb19280477362b5ab0f04e1a75c9ffc1020c3
Aleksandr Shenshin <shenshin@me.com>
2021-03-17 05:21:35 +0100

    Move sample data from bundlySeeder.js to data/bundles.js

--- backend/bundlySeeder.js
+++ backend/bundlySeeder.js
@@ -2,6 +2,7 @@
 import dotenv from 'dotenv';
 import colors from 'colors';
 
+import { farmersDataSet, foodItemsDataSet, bundlesDataSet } from './data/bundles.js';
 import Farmer from './models/farmerModel.js';
 import FoodItem from './models/foodItemModel.js';
 import Bundle from './models/bundleModel.js';
@@ -17,65 +18,20 @@ const importData = async () => {
     await FoodItem.deleteMany();
     await Bundle.deleteMany();
 
-    const farmers = await Farmer.insertMany([
-      {
-        name: 'John Smith',
-        city: 'Eindhoven',
-        address: 'Churchillaan 10',
-        image: 'farm-1.jpg',
-        description: 'Great farm in some asshole of the world',
-      },
-      {
-        name: 'Martijn Jansen',
-        city: 'Nuenen',
-        address: 'Europalaan 25',
-        image: 'farm-2.jpg',
-        description: 'great farm in the ...',
-      },
-    ]);
+    const farmers = await Farmer.insertMany(farmersDataSet);
 
-    const foodItems = await FoodItem.insertMany([
-      {
-        name: 'Apples',
-        price: 1.0,
-        image: 'image-1.jpg',
-        description: 'tasty apples',
-        farmer: farmers[0],
-      },
-      {
-        name: 'Strawberry',
-        price: 1.2,
-        image: 'image-2.jpg',
-        description: 'Tasty berries',
-        farmer: farmers[1],
-      },
-    ]);
+    // assuming that the lengths of farmers and fooditems are equal
+    const foodItems = await FoodItem.insertMany(foodItemsDataSet.map((product, index) => ({
+      ...product,
+      farmer: farmers[index],
+    })));
 
-    await Bundle.insertMany([
-      {
-        name: 'Fruits',
-        foodItems,
-        image: 'bundle-image-1.jpg',
-        description: 'Tasty Fruits',
-        category: 'Vegetarian',
-        rating: 4.6,
-        numReviews: 99,
-        price: 13.1,
-        countInStock: 145,
-      },
-      {
-        name: 'Veggies',
-        foodItems,
-        image: 'bundle-image-2.jpg',
-        description: 'Tasty Veggies',
-        category: 'Vegan',
-        rating: 4.3,
-        numReviews: 123,
-        price: 11.9,
-        countInStock: 111,
-      },
-    ]);
-    const bundles = await Bundle.find().populate('foodItems');
+    await Bundle.insertMany(bundlesDataSet.map((bundle) => ({
+      ...bundle,
+      foodItems,
+    })));
+
+    const bundles = await Bundle.find();
     console.log(bundles);
     console.log('Data Imported!'.green.inverse);
     process.exit();
---
+++ backend/data/bundles.js
@@ -0,0 +1,52 @@
+export const farmersDataSet = [
+  {
+    name: 'John Smith',
+    city: 'Eindhoven',
+    address: 'Churchillaan 10',
+    image: 'farm-1.jpg',
+    description: 'Great farm in some asshole of the world',
+  },
+  {
+    name: 'Martijn Jansen',
+    city: 'Nuenen',
+    address: 'Europalaan 25',
+    image: 'farm-2.jpg',
+    description: 'great farm in the ...',
+  },
+];
+export const foodItemsDataSet = [
+  {
+    name: 'Apples',
+    price: 1.0,
+    image: 'image-1.jpg',
+    description: 'tasty apples',
+  },
+  {
+    name: 'Strawberry',
+    price: 1.2,
+    image: 'image-2.jpg',
+    description: 'Tasty berries',
+  },
+];
+export const bundlesDataSet = [
+  {
+    name: 'Fruits',
+    image: 'bundle-image-1.jpg',
+    description: 'Tasty Fruits',
+    category: 'Vegetarian',
+    rating: 4.6,
+    numReviews: 99,
+    price: 13.1,
+    countInStock: 145,
+  },
+  {
+    name: 'Veggies',
+    image: 'bundle-image-2.jpg',
+    description: 'Tasty Veggies',
+    category: 'Vegan',
+    rating: 4.3,
+    numReviews: 123,
+    price: 11.9,
+    countInStock: 111,
+  },
+];
